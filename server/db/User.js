da25b03325aed8d7da88197991fadc3cdad5a3dd
irBug <irwingalvarez@gmail.com>
2022-08-16 09:26:26 -0500

    everything except the extra credit

--- server/db/User.js
+++ server/db/User.js
@@ -3,9 +3,104 @@ const db = require('./db');
 
 const User = db.define('user', {
   // Add your Sequelize fields here
-
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: { notEmpty: true },
+  },
+  userType: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: 'STUDENT',
+    validate: {
+      studentOrTeacherValidator(value) {
+        if (value === 'STUDENT') {
+          return;
+        }
+        if (value === 'TEACHER') {
+          return;
+        }
+        throw 'userType Error!';
+      },
+    },
+  },
+  isStudent: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      if (this.userType === 'STUDENT') {
+        return true;
+      }
+      return false;
+    },
+  },
+  isTeacher: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      if (this.userType === 'TEACHER') {
+        return true;
+      }
+      return false;
+    },
+  },
 });
 
+User.findUnassignedStudents = async function () {
+  return await User.findAll({
+    where: {
+      [Sequelize.Op.and]: [{ mentorId: null }, { userType: 'STUDENT' }],
+    },
+  });
+};
+
+User.findTeachersAndMentees = async function () {
+  return await User.findAll({
+    where: {
+      userType: 'TEACHER',
+    },
+    include: 'mentees',
+  });
+};
+
+// //I could not get this hook to NOT break eveything else and i don't know what i'm doing wrong. I cannot access theTEACHER with the id that is trying to be assigned as the mentees' mentorId
+// User.addHook('beforeUpdate', async (userInstance) => {
+//   const mentor = await User.findOne({
+//     where: {
+//       [Sequelize.Op.and]: [
+//         { id: userInstance.mentorId },
+//         { userType: 'TEACHER' },
+//       ],
+//     },
+//   });
+
+//   if (mentor.userType === 'STUDENT') {
+//     throw 'Cannot update a user with a mentor who is not a TEACHER!';
+//   } else return;
+// });
+
+User.addHook('beforeUpdate', (userInstance) => {
+  if (userInstance.userType === 'TEACHER' && userInstance.mentorId > 0) {
+    throw 'Cannot change userType from STUDENT to TEACHER when user has a mentor';
+  } else return;
+});
+
+User.addHook('beforeUpdate', async (userInstance) => {
+  let listOfUsersWithMentees = await User.findTeachersAndMentees();
+  listOfUsersWithMentees = listOfUsersWithMentees
+    .filter((userObj) => {
+      return userObj.dataValues.mentees.length > 0;
+    })
+    .map((userObj) => {
+      return userObj.name;
+    });
+
+  if (
+    userInstance.userType === 'STUDENT' &&
+    listOfUsersWithMentees.includes(userInstance.name)
+  ) {
+    throw 'cannot change userType from TEACHER to STUDENT when user has mentees';
+  } else return;
+});
 
 /**
  * We've created the association for you!
--- server/routes/users.js
+++ server/routes/users.js
@@ -18,4 +18,77 @@ const {
 
 // Add your routes here:
 
+router.get('/unassigned', async (req, res, next) => {
+  try {
+    const response = await User.findUnassignedStudents();
+    res.status(200).send(response);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/teachers', async (req, res, next) => {
+  try {
+    const response = await User.findTeachersAndMentees();
+    res.status(200).send(response);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  let id = req.params.id;
+  try {
+    if (/(\d+)/.test(id)) {
+      const userToDelete = await User.findOne({
+        where: { id: id },
+      });
+
+      if (userToDelete) {
+        await User.destroy({ where: { id: id } });
+        res.sendStatus(204);
+      } else {
+        res.sendStatus(404);
+      }
+    } else {
+      res.sendStatus(400);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const count = await User.count({
+      where: { name: req.body.name },
+    });
+
+    if (count > 0) {
+      res.sendStatus(409);
+      return;
+    }
+    const response = await User.create(req.body);
+    res.status(201).send(response);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const toUpdate = await User.findOne({
+      where: { id: req.params.id },
+    });
+    if (!toUpdate) {
+      res.sendStatus(404);
+      return;
+    }
+    const updatedUser = await toUpdate.update(req.body);
+    res.status(200).send(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
